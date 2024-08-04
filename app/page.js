045a860b27c5629374dbc382d49bab81af9e24ce
Jason Irie <40525329+Shoheicode@users.noreply.github.com>
2024-08-04 00:13:13 -0700

    updated page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Modal, Stack, TextField, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
-import { query } from "firebase/firestore";
+import { query,collection, getDocs, getDoc, doc } from "firebase/firestore";
 
 const items = [
   'tomato',
@@ -34,12 +34,43 @@ export default function Home() {
       });
     })
     setInventory(inventoryList)
-    console.log("inventory")
+  }
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity+1})
+    }else{
+      await setDoc(docRef, {quantity: 1})
+    }
+    await updateInventory()
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      if(quantity === 1){
+        await deleteDoc(docRef)
+      } else{
+        await setDoc(docRef, {quantity: quantity-1})
+      }
+    }
+
+    await updateInventory()
   }
 
   useEffect(()=>{
     updateInventory()
   }, [])
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
   return <Box 
     width = "100vw"
@@ -53,12 +84,45 @@ export default function Home() {
     <Box border={'1px solid #333'}>
       {
         inventory.forEach((item) =>{
-          return (<>
+          return (
+          <>
             {item.name}
-            {item.count}
-          </>)
+            {item.quantity}
+          </>
+          )
         })
       }
+      <Modal open = {open} onClose={handleClose}>
+        <Box 
+          position='absolute' 
+          top='50%' 
+          left='50%' 
+          textTransform = 'translate(-50%, -50%'
+          width={400}
+          bgcolor= "blue"
+          border = "2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+        >
+          <Typography
+            variant="h6"
+          >
+            Add Item
+          </Typography>
+          <Stack
+            width="100%"
+            direction="row"
+            spacing={2}
+          >
+            <TextField>
+              
+            </TextField>
+          </Stack>
+        </Box>
+      </Modal>
       <Box
         width='800px'
         height='100px'
