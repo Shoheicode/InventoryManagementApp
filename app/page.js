c2e0d17c1383664dd46cfad8b52b89e099212346
Jason Irie <40525329+Shoheicode@users.noreply.github.com>
2024-08-04 14:58:51 -0700

    updated and added firebase

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import { Box, Modal, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Modal, Stack, TextField, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
-import { firestore } from "@/firebase";
-import { query,collection, getDocs, getDoc, doc } from "firebase/firestore";
+import { firestore } from "@/app/firebase";
+import { query,collection, getDocs, getDoc, doc, setDoc, deleteDoc } from "firebase/firestore";
 
 const items = [
   'tomato',
@@ -21,10 +21,13 @@ const items = [
 export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
-  const [item, setItemName] = useState('')
+  const [itemName, setItemName] = useState('')
+  const style = {
+    transform: 'translate(-50%, -50%)',
+  }
 
   const updateInventory = async () =>{
-    const snapShot = query(collection(firestore, 'pantry'))
+    const snapShot = query(collection(firestore, 'inventory'))
     const docs = await getDocs(snapShot)
     const inventoryList = []
     docs.forEach((doc) =>{
@@ -37,7 +40,7 @@ export default function Home() {
   }
 
   const addItem = async (item) => {
-    const docRef = doc(collection(firestore, 'pantry'), item)
+    const docRef = doc(collection(firestore, 'inventory'), item)
     const docSnap = await getDoc(docRef)
 
     if(docSnap.exists()){
@@ -50,7 +53,7 @@ export default function Home() {
   }
 
   const removeItem = async (item) => {
-    const docRef = doc(collection(firestore, 'pantry'), item)
+    const docRef = doc(collection(firestore, 'inventory'), item)
     const docSnap = await getDoc(docRef)
 
     if(docSnap.exists()){
@@ -76,13 +79,12 @@ export default function Home() {
     width = "100vw"
     height = "100vh"
     display={"flex"}
-    justifyContent = {"center"}
+    justifyContent = {"space-around"}
     alignItems = {"center"}
     flexDirection={'column'}
     bgcolor={"#8aecff"}
   >
-    <Box border={'1px solid #333'}>
-      {
+    {
         inventory.forEach((item) =>{
           return (
           <>
@@ -92,14 +94,20 @@ export default function Home() {
           )
         })
       }
-      <Modal open = {open} onClose={handleClose}>
-        <Box 
+      
+      <Modal 
+        open = {open} 
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}
           position='absolute' 
           top='50%' 
           left='50%' 
-          textTransform = 'translate(-50%, -50%'
+          transform = 'translate(-50%, -50%)'
           width={400}
-          bgcolor= "blue"
+          bgcolor= 'white'
           border = "2px solid #000"
           boxShadow={24}
           p={4}
@@ -109,6 +117,8 @@ export default function Home() {
         >
           <Typography
             variant="h6"
+            id="modal-modal-title" 
+            component="h2"
           >
             Add Item
           </Typography>
@@ -117,34 +127,59 @@ export default function Home() {
             direction="row"
             spacing={2}
           >
-            <TextField>
+            <TextField
+              id="outlined-basic"
+              label="Item"
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            >
               
             </TextField>
+            <Button 
+              variant="outlined"
+              onClick={ () => {
+                  addItem(itemName)
+                  setItemName('')
+                  handleClose()
+                }  
+              }
+            >
+              Add
+            </Button>
           </Stack>
         </Box>
       </Modal>
+
+      <Box>
+        <Button variant="contained" onClick={handleOpen}>
+          Add New Item
+        </Button>
+      </Box>
+    <Box border={'1px solid #333'}>
       <Box
         width='800px'
         height='100px'
         bgcolor={'#7c82ff'}
       >
         <Typography variant="h2" color={'#333'} textAlign={'center'}>
-          Pantry Items
+          Inventory Items
         </Typography>
 
       </Box>
       <Stack width = "800px" height = "400px" spacing = {2} overflow={'auto'}>
-        {items.map((i)=>(
+        {inventory.map(({name, quantity})=>(
           <Box 
-            key = {i}
+            key = {name}
             width = "750px"
             height = "200px"
             display = {"flex"}
-            justifyContent={'center'}
+            justifyContent={'space-between'}
             alignItems={'center'}
             textAlign={'center'}
             bgcolor={'$f0f0f0'}
-
+            padding={5}
           >
             <Typography
               variant = {"h4"}
@@ -153,9 +188,20 @@ export default function Home() {
               fontWeight={'bold'}
             >
               {
-                i.charAt(0).toUpperCase() + i.slice(1)
+                name.charAt(0).toUpperCase() + name.slice(1)
               }
             </Typography>
+            <Typography
+              variant = {"h4"}
+              color = {'#333'}
+              textAlign={'center'}
+              fontWeight={'bold'}
+            >
+              Quantity: {quantity}
+            </Typography>
+            <Button variant="contained" onClick={() => removeItem(name)}>
+              X
+            </Button>
           </Box>
         ))
         }
